87c5b2e5e2bf5771d8c0c0fcd22476ca5dd2c977
vodounoukpe <fvodounoupke@gmail.com>
2025-02-19 14:07:24 +0100

    Importation de la cart bancais
    initialisation de la page wallet

---
+++ apt2/lib/pages/wallet.dart
@@ -0,0 +1,76 @@
+import 'package:u_credit_card/u_credit_card.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const WalletPage(),
+    );
+  }
+}
+
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Wallets")),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          Center(
+            child: CreditCardUi(
+              width: 300,
+              cardHolderFullName: 'John Doe',
+              cardNumber: '1234567812345678',
+              validFrom: '01/23',
+              validThru: '01/28',
+              topLeftColor: Colors.blue,
+              doesSupportNfc: true,
+              placeNfcIconAtTheEnd: true,
+              cardType: CardType.debit,
+              cardProviderLogo: FlutterLogo(),
+              cardProviderLogoPosition: CardProviderLogoPosition.right,
+              showBalance: true,
+              balance: 128.32434343,
+              autoHideBalance: true,
+              enableFlipping: true,
+              cvvNumber: '123',
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(children: [Text("Total spending")]),
+          SizedBox(height: 10,),
+          Container(
+                width: screenWidth * 1,
+                height: screenHeight * 0.1,
+                color: Colors.grey.withAlpha(50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Day"),
+                    Text("Week"),
+                    Text("Month"),
+                    Text("Custom rang"),
+                  ],
+                ),
+              ),
+        ],
+      ),
+    );
+  }
+}
